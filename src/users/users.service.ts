88d25b008b6033c0597470423782810f2c5320db
mohansharma916 <mohansharma916@gmail.com>
2023-08-31 19:22:22 +0530

    ok

---
+++ src/users/users.service.ts
@@ -0,0 +1,58 @@
+import { PrismaService } from 'nestjs-prisma';
+import { Injectable, BadRequestException } from '@nestjs/common';
+import { PasswordService } from '../auth/password.service';
+import { ChangePasswordInput } from './dto/change-password.input';
+import { UpdateUserInput } from './dto/update-user.input';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    private prisma: PrismaService,
+    private passwordService: PasswordService,
+  ) {}
+
+  updateUser(userId: string, newUserData: UpdateUserInput) {
+    return this.prisma.user.update({
+      data: newUserData,
+      where: {
+        id: userId,
+      },
+    });
+  }
+
+  createUser(newUserData: UpdateUserInput) {
+    return this.prisma.enquiryDetails.create({
+      data: newUserData,
+    });
+  }
+
+  async changePassword(
+    userId: string,
+    userPassword: string,
+    changePassword: ChangePasswordInput,
+  ) {
+    const passwordValid = await this.passwordService.validatePassword(
+      changePassword.oldPassword,
+      userPassword,
+    );
+
+    if (!passwordValid) {
+      throw new BadRequestException('Invalid password');
+    }
+
+    const hashedPassword = await this.passwordService.hashPassword(
+      changePassword.newPassword,
+    );
+
+    return this.prisma.user.update({
+      data: {
+        password: hashedPassword,
+      },
+      where: { id: userId },
+    });
+  }
+
+  getAllUsers() {
+    return this.prisma.user.findMany({});
+  }
+}
